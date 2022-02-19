a7ca70904cb8b0f310f170d8c9c021d5d3ebb5f9
coder-18m <76606410+coder-18m@users.noreply.github.com>
2022-02-19 01:35:27 -0800

    major updates and bug fixes

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var factList = ['An estimated 25 percent of all marine life, including over 4,000 species of fish, are dependent on coral reefs at some point in their life cycle', 'The ocean has absorbs about 30-40% of the carbon dioxide (CO2) that has been released into the atmosphere', 'The amount of CO2 in the atmosphere has increased more than 20% in less than 40 years, owing largely to human activities, and representing well over 50% of the total increase in atmospheric carbon dioxide since the onset of the industrial revolution (1750)', 'In 2013, carbon dioxide in the atmosphere passed 400 parts per million (ppm)—higher than at any time in the last one million years']
+var factList = ['Rest in peace, boiling water, you will be mist',"Two chemists go into a bar. The first one says: I think I'll have an H2O. The second one says: I think I'll have an H2O too — and he died.",
+'I had to make these bad chemistry jokes because all the good ones Argon.', "He threw sodium chloride at me! That\'s a salt!", 'No matter how popular they get, antibiotics will never go viral']
 
 var fact = document.getElementById("fact");
 var factBtn = document.getElementById("factBtn");
